c24f45dc3f12f7be8dfa3dd09840773999d46a7d
bguz1kk <bguz1kk@gmail.com>
2025-11-23 17:46:49 +0100

    Test 5

--- src/main/java/pl/wsb/fitnesstracker/userevent/UserEvent.java
+++ src/main/java/pl/wsb/fitnesstracker/userevent/UserEvent.java
@@ -4,11 +4,19 @@ import jakarta.persistence.Column;
 import jakarta.persistence.FetchType;
 import jakarta.persistence.JoinColumn;
 import jakarta.persistence.ManyToOne;
+import lombok.AccessLevel;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.ToString;
 import pl.wsb.fitnesstracker.event.Event;
 import pl.wsb.fitnesstracker.user.api.User;
 import jakarta.persistence.*;
 
-
+@Entity
+@Table(name = "user_event")
+@Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@ToString
 public class UserEvent {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     @Id
